37b7d66af1606fe73d93f5c926c31f2dc858a540
mengheng <havmengheng111@gmail.com>
2023-08-29 22:18:31 +0700

    almost a full fitness app

--- app/src/main/java/com/example/fitness_optimal/MainActivity.kt
+++ app/src/main/java/com/example/fitness_optimal/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.fitness_optimal
 
+import android.icu.text.CaseMap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
@@ -7,6 +8,8 @@ import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.cardview.widget.CardView
 import androidx.drawerlayout.widget.DrawerLayout
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
 
 import com.google.android.material.navigation.NavigationView
 
@@ -18,7 +21,7 @@ class MainActivity : AppCompatActivity() {
 
 
     lateinit var toggle: ActionBarDrawerToggle
-
+    lateinit var drawerLayout: DrawerLayout
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,27 +29,27 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         //navbar
-        var drawerLayout : DrawerLayout = findViewById(R.id.drawerLayout)
+        drawerLayout  = findViewById(R.id.drawerLayout)
         var navView : NavigationView = findViewById(R.id.nav_view)
 
 
 
         toggle = ActionBarDrawerToggle(this,drawerLayout,R.string.open,R.string.close)
-
        drawerLayout.addDrawerListener(toggle)
        toggle.syncState()
 
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
         navView.setNavigationItemSelectedListener {
+            it.isChecked = true
 
             when(it.itemId){
-                R.id.nav_Workout -> Toast.makeText(applicationContext,"Clicked Workout",Toast.LENGTH_SHORT).show()
-                R.id.nav_Contect-> Toast.makeText(applicationContext,"Clicked Contect us",Toast.LENGTH_SHORT).show()
-                R.id.nav_setting -> Toast.makeText(applicationContext,"Clicked setting",Toast.LENGTH_SHORT).show()
-                R.id.nav_login -> Toast.makeText(applicationContext,"Clicked login",Toast.LENGTH_SHORT).show()
-                R.id.nav_share -> Toast.makeText(applicationContext,"Clicked share",Toast.LENGTH_SHORT).show()
-                R.id.nav_review -> Toast.makeText(applicationContext,"Clicked review",Toast.LENGTH_SHORT).show()
+                R.id.nav_home -> replaceFragment(HomeFragment(), it.title.toString())
+                R.id.nav_Workout -> replaceFragment(WorkoutFragment(), it.title.toString())
+                R.id.nav_about -> replaceFragment(aboutFragment(), it.title.toString())
+                R.id.nav_Diet -> replaceFragment(DietFragment(), it.title.toString())
+                R.id.nav_contact -> replaceFragment(ContactFragment(), it.title.toString())
+
 
 
             }
@@ -54,11 +57,21 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    private fun replaceFragment(fragment: Fragment,title :String){
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.framelayout,fragment)
+        fragmentTransaction.commit()
+        drawerLayout.closeDrawers()
+        setTitle(title)
+    }
+
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         if (toggle.onOptionsItemSelected(item)){
 
             return true
         }
+
 
 
         return super.onOptionsItemSelected(item)
